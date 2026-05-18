b5faaa02bdcb37bf50056120309e67ea5cd1d8ec
Luciano Guerra <lucianobguerra@gmail.com>
2026-05-18 16:35:45 -0300

    Creacion de la sidebar. Fue añadida a cada view

--- proyecto-mundial/src/views/LlaveDeEliminacion.vue
+++ proyecto-mundial/src/views/LlaveDeEliminacion.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
+      <Sidebar />
+      
     <h1>LLAVE DE ELIMINACIÓN</h1>
     <p>Vista de cruces eliminatorios</p>
   </div>
 </template>
 
 <script setup>
+  import Sidebar from '@/components/Sidebar.vue';
 
 </script>
 
--- proyecto-mundial/src/views/RankingGlobalView.vue
+++ proyecto-mundial/src/views/RankingGlobalView.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
+      <Sidebar />
+
     <h1>RANKING GLOBAL</h1>
     <p>Tabla de posiciones de usuarios</p>
   </div>
 </template>
 
 <script setup>
+  import Sidebar from '@/components/Sidebar.vue';
 
 </script>
 
